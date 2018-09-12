029df735b333afd786e709293bf488097260d5dd
Yehor <novakov.eo@gmail.com>
2018-09-12 13:10:24 +0300

    `21

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -6,5 +6,6 @@ handlers.checkEmail = function(args, context)
 	var data = response["Data"];//здесь получаю нормальную строку
 	log.debug("data: " + JSON.stringify(data) + " of type: " + typeof response);
 	var emails = data["RegisteredEmails"];//хочу получить массив по ключу, правильно все написал, но возвращает ничего
+	log.debug("emails: " + JSON.stringify(emails) + " of type: " + typeof response);
     return emails;
 }
