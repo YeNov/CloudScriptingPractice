c1c199b7a61fb7a2528a3bfe54553acc866f0381
Yehor <novakov.eo@gmail.com>
2018-09-12 15:56:15 +0300

    hz

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -6,21 +6,10 @@ handlers.checkEmail = function(args, context)
 	var emails = data["RegisteredEmails"];
 	log.debug("emails: " + JSON.stringify(emails));
 	
-	var emailsStr = JSON.stringify(emails);
-	var playerEmail = args['email'];	
-<<<<<<< HEAD
-	var emailsCollision = emailsStr.includes(playerEmail);
-	return {emails};
-	emailsStr['emails'].push(playerEmail);
-	log.debug("push: " + JSON.stringify(emails));
-=======
-	var emailsCollision = JSON.stringify(emails).includes(playerEmail);
 	
-	var obj = JSON.parse(emails);
-	obj['emails'].push(playerEmail);
-	jsonStr = JSON.stringify(obj);
-	log.debug("push: " + JSON.stringify(jsonStr));
->>>>>>> parent of 08ca6c2... 123
+	var emailsCollision = emailsStr.includes(playerEmail);
+	emails.push(playerEmail);
+	log.debug("push: " + JSON.stringify(emails));
 
 	request = {
 		key: "RegisteredEmails",
