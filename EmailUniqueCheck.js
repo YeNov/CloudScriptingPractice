4ca53cbbbce94d0edb778342346a4f8d8f116016
Yehor <novakov.eo@gmail.com>
2018-09-12 13:08:11 +0300

    123

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -2,9 +2,9 @@ handlers.checkEmail = function(args, context)
 {
 	var request = ["RegisteredEmails"];
 	var response = server.GetTitleData(request);//жсон
-	log.debug("response: " + response.toString() + " of type: " + typeof response);
+	log.debug("response: " + JSON.stringify(response) + " of type: " + typeof response);
 	var data = response["Data"];//здесь получаю нормальную строку
-	log.debug("data: " + data.toString() + " of type: " + typeof response);
+	log.debug("data: " + JSON.stringify(data) + " of type: " + typeof response);
 	var emails = data["RegisteredEmails"];//хочу получить массив по ключу, правильно все написал, но возвращает ничего
     return emails;
 }
