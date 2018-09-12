f00c79bc014c6373aad89f4aa64c29398e4b387d
Yehor <novakov.eo@gmail.com>
2018-09-12 15:53:53 +0300

    Revert "123"
    This reverts commit 08ca6c24558878fe2411dd43c60e3241cd3eb708.

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -8,10 +8,19 @@ handlers.checkEmail = function(args, context)
 	
 	var emailsStr = JSON.stringify(emails);
 	var playerEmail = args['email'];	
+<<<<<<< HEAD
 	var emailsCollision = emailsStr.includes(playerEmail);
 	return {emails};
 	emailsStr['emails'].push(playerEmail);
 	log.debug("push: " + JSON.stringify(emails));
+=======
+	var emailsCollision = JSON.stringify(emails).includes(playerEmail);
+	
+	var obj = JSON.parse(emails);
+	obj['emails'].push(playerEmail);
+	jsonStr = JSON.stringify(obj);
+	log.debug("push: " + JSON.stringify(jsonStr));
+>>>>>>> parent of 08ca6c2... 123
 
 	request = {
 		key: "RegisteredEmails",
