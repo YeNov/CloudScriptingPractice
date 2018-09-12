8ec9730ca7ceb0f9eaafb72624fd632e1fac69da
Yehor <novakov.eo@gmail.com>
2018-09-12 12:02:20 +0300

    213

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -2,7 +2,7 @@ handlers.checkEmail = function(args, context)
 {
 	var request = ["RegisteredEmails"];
 	var response = server.GetTitleData(request);
-	var data = JSON.parse(response);
-	var emails = data.RegisteredEmails;
-    return JSON.stringify(data);
+	var data = response["Data"];
+	var emails = data["RegisteredEmails"];
+    return emails;
 }
