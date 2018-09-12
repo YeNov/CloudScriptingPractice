bce60501a36e73b7f4969488a2b0568bb886e8d2
Yehor <novakov.eo@gmail.com>
2018-09-12 13:32:44 +0300

    Заглянем в аргумент

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -2,10 +2,8 @@ handlers.checkEmail = function(args, context)
 {
 	var request = ["RegisteredEmails"];
 	var response = server.GetTitleData(request);//жсон
-	log.debug("response: " + JSON.stringify(response) + " of type: " + typeof response);
+	log.debug("args: " + JSON.stringify(args));
 	var data = response["Data"];//здесь получаю нормальную строку
-	log.debug("data: " + JSON.stringify(data) + " of type: " + typeof response);
 	var emails = data["RegisteredEmails"];//хочу получить массив по ключу, правильно все написал, но возвращает ничего
-	log.debug("emails: " + JSON.stringify(emails) + " of type: " + typeof response);
     return {responseContent: emails};
 };
