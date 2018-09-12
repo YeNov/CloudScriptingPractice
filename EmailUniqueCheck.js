033e8b7420f77bc261167beddbf264289b4610b3
Yehor <novakov.eo@gmail.com>
2018-09-12 11:33:19 +0300

    Попробуем парсер

--- EmailUniqueCheck.js
+++ EmailUniqueCheck.js
@@ -2,7 +2,7 @@ handlers.checkEmail = function(args, context)
 {
 	var request = ["RegisteredEmails"];
 	var response = server.GetTitleData(request);
-	var data = response['Data'];
-	var emails = data['RegisteredEmails'];
+	var data = JSON.Parse(response);
+	var emails = data.emails;
     return emails;
 }
